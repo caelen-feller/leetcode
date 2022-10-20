00611d6ca9d37177bdbef42340d974bf8c423abe
Caelen Feller <fellerc@tcd.ie>
2022-10-20 11:17:07 +0000

    Sync LeetCode submission - House Robber (cpp)

--- problems/house_robber/solution.cpp
+++ problems/house_robber/solution.cpp
@@ -6,6 +6,7 @@ public:
         // 1 2 3 1 -> 1 2 4 3
         // 2 7 9 3 1 -> 2 7 11 10 12
         if(n >= 3) nums[2] += nums[0];
+
         for(int i = 3; i < n; i++) {
             nums[i] += max(nums[i-2], nums[i-3]);
         }
